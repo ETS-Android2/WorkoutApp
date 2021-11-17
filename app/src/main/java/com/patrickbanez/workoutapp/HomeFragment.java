38f7d96029dddec9fdc1799be1429c80d708cea3
DAVID CHANJE WOO <47830372+wcj617@users.noreply.github.com>
2021-11-17 00:46:32 -0800

    dd

--- app/src/main/java/com/patrickbanez/workoutapp/HomeFragment.java
+++ app/src/main/java/com/patrickbanez/workoutapp/HomeFragment.java
@@ -19,59 +19,5 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends AppCompatActivity {
-    private List<String> items = Arrays.asList("ABS", "Chest", "Arm", "Leg", "Shoulder");
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Intent home = new Intent(this, CreateUserActivity.class);
-        setContentView(R.layout.fragment_home);
-
-        SearchView searchView = findViewById(R.id.Search_view);
-        TextView resultTextView = findViewById(R.id.textView);
-        resultTextView.setText(getResult());
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                resultTextView.setText(search(newText));
-                return true;
-            }
-
-
-        });
-
-    }
-
-    private String getResult() {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < items.size(); i++) {
-            String item = items.get(i);
-            sb.append(item);
-            if (i != items.size() - 1) {
-                sb.append("\n");
-            }
-        }
-        return sb.toString();
-    }
-    private String search(String query) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < items.size(); i++) {
-            String item = items.get(i);
-            if (item.toLowerCase().contains(query.toLowerCase())) {
-                sb.append(item);
-                if (i != items.size() - 1) {
-                    sb.append("\n");
-                }
-            }
-        }
-        return sb.toString();
-    }
 }
